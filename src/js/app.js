fee1549d5fbb8dedb9d74891af73724da8b46902
Anny <anny@juksy.com>
2017-04-05 16:03:13 +0800

    base

--- src/js/app.js
+++ src/js/app.js
@@ -17,4 +17,14 @@ import '../img/android-chrome-144.png';
 import '../img/android-chrome-192.png';
 import '../img/manifest.json';
 
+// index slider import images
+import '../img/index/1500x500_1.jpg';
+import '../img/index/1500x500_2.jpg';
+
+// Main CSS
 const css = require('../scss/app.scss');
+
+// Main JS
+import indexSlider from './index/indexSlider.js';
+
+indexSlider();
