7e6ca63e18b580a74c11768b2d9c8205609117c1
Anny <anny@juksy.com>
2017-04-12 19:29:25 +0800

    LATEST最新文章 串接API sandbox 改成 https

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -48,7 +48,7 @@
   // API get data
   function latestGet ($size, $from) {
     $.ajax({
-      url: 'http://juksy.getsandbox.com/v1.0/articles/latest',
+      url: 'https://juksy.getsandbox.com/v1.0/articles/latest',
       data: { size: $size, from: $from },
       type: 'GET',
       dataType: 'json',
