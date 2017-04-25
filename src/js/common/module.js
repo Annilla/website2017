2f4554d4faa520f68b8f70f069f0d0ce2630655b
Anny <anny@juksy.com>
2017-04-25 15:20:52 +0800

    測試手機廣告

--- src/js/common/module.js
+++ src/js/common/module.js
@@ -1,5 +1,14 @@
 let vLazyLoad;
 let iframeLazyLoad;
+let JUKSY = JUKSY || { pageType: null,  isCollection: false,  is18up: false };
+
+export function initJUKSY(obj, key, def) {
+  $(function() {
+    JUKSY.pageType = $('meta[property="juksy:type"]').attr('content');
+    JUKSY.is18up = !!($('meta[property="juksy:adult"]').attr('content') === 'true');
+    JUKSY.isCollection = !!($('meta[property="juksy:collection"]').attr('content') === 'true');
+  });
+}
 
 /**
  * Array/Object siample getter with default value.
@@ -40,6 +49,36 @@ export function toDateString(timestamp) {
   return `${assoc[1]} ${assoc[2]}, ${assoc[3]}`;
 }
 
+export function googledfp() {
+  let winW = $(window).width();
+  function showDFP(element_id) {
+    let $adUnit = $('#' + element_id);
+    console.debug('Loading DFP:', element_id);
+
+    googletag.cmd.push(function() {
+      googletag.pubads().display(
+        $adUnit.data('adunit'),
+        $adUnit.data('dimensions'),
+        element_id);
+    });
+  }
+  $('[data-module="googledfp"]').each(function () {
+    let $this = $(this);
+    let device = $this.data('device');
+    let element_id = this.id;
+
+    // Remove DFP if isCollection = true 
+    if (JUKSY.isCollection) { $this.remove(); }
+    // Show DFP by device width
+    if (winW < 768 && device === 'mobile') {
+      showDFP(element_id);
+    }
+
+  });
+}
+
 // Initial
+initJUKSY();
 vLazyLoad_init();
 dotdotdot();
+googledfp();
