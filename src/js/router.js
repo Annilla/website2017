ad106c6ee882d9983980760585a9650e032338a6
Anny <anny@juksy.com>
2017-04-28 10:30:52 +0800

    修正 router 輸出

--- src/js/router.js
+++ src/js/router.js
@@ -1,10 +1,12 @@
 import { Router } from 'director/build/director.js';
 
+// npm run prod use: /website2017/dist
+
 const routes = {
   // 首頁
-  '/': require('./pages/index')['default'],
+  '/website2017/dist/': require('./pages/index')['default'],
   // 文章內容頁
-  '/article_:type.html': [
+  '/website2017/dist/article_:type.html': [
     require('./pages/article')['default'],
     () => {
       require('./common/googledfp.js');
