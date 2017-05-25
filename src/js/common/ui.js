c00a5e42fb724ee5b6220fc0b6a2351e86f2e672
Anny <anny@juksy.com>
2017-05-25 11:20:02 +0800

    調整 nav 動畫和 fixed 動作

--- src/js/common/ui.js
+++ src/js/common/ui.js
@@ -1,3 +1,27 @@
+import {throttle} from './module.js';
+
+function navFixed() {
+  let $window = $(window);
+  let winH = $window.height();
+  let fixed = 'fixed';
+  let $nav = $('.header .nav');
+  let $navH = $nav.height();
+  let $navGroup = $nav.find('.navGroup');
+
+  function fixedNAV() {
+    let window_position = $window.scrollTop();
+    let navTop = $nav.offset() ? $nav.offset().top : 0;
+    if (window_position > navTop) {
+      $navGroup.addClass(fixed);
+    }
+    else if (window_position < navTop + $navH) {
+      $navGroup.removeClass(fixed);
+    }
+  }
+
+  $window.scroll(throttle(fixedNAV, 250));
+}
+
 function searchCover() {
   // Click black background to hide search page
   let $bg = $('nav');
@@ -9,5 +33,8 @@ function searchCover() {
 }
 
 export function ui() {
+  // Nav fixed top
+  navFixed();
+  // Search popup
   searchCover();
 }
