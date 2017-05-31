bca06a84062d19989767c043cdd37e179bf9261f
Anny <anny@juksy.com>
2017-05-31 18:12:00 +0800

    品牌館 和 搜尋頁 改用 es7 async await 寫法

--- src/js/pages/brand/nextPage.js
+++ src/js/pages/brand/nextPage.js
@@ -5,8 +5,8 @@ let $channel = $('.brand');
 let $more = $channel.find('.more');
 let $articles = $channel.find('.braArticles');
 let $tag = $channel.find('.intro h1.title');
-let braFrom = 0;
 let braSize = 6;
+let braFrom = 0 - braSize; // fixed for first time loading
 let braTag = [$.trim($tag.text())];
 let lazyLoad;
 
@@ -30,70 +30,72 @@ function makeBrandItem(data) {
   `;
 }
 
-function braGet(q, size, from) {
-  axios.get(`//${API_HOST}/v1/search/tags`, {
+async function braGet(q, size, from) {
+  const {data} = await axios.get(`//${API_HOST}/v1/search/tags`, {
     params: {
       q: q,
       size: size,
       from: from
     }
-  })
-  .then(function (response) {
-    // API set data
-    let data = response.data;
-    let length = data.length;
-    let $list = $articles.find('li');
-    for (let i = 0; i < length; i++) {
-      let tmpImg = `
-        <img data-original="${data[i].image_cover}" width="100%" height="auto" alt="${data[i].title}">
-        <div class="author hide show-lg">${toDateString(data[i].published_at)} | by ${data[i].author}</div>
-      `;
-      let tmpDetail = `
-        <div class="date hide-md">${toDateString(data[i].published_at)}</div>
-        <a class="title" data-module="dotdotdot" href="${data[i].link}" target="_blank">
-          <h3>${data[i].title}</h3>
-        </a>
-      `;
-
-      let itemIndex = braFrom + i;
-      $list.eq(itemIndex).replaceWith(makeBrandItem({
-        link: data[i].link,
-        cover: tmpImg,
-        detail: tmpDetail
-      }));
-    }
-    dotdotdot();
-    lazyLoad.update();
-    // If length < braSize, delete unnecessary skeleton
-    if (length < braSize) {
-      for (let j = length; j < braSize; j++) {
-        let itemIndex = braFrom*braSize + j;
-        $list.eq(itemIndex).remove();
-      }
-      $more.hide();
-    }
-    else {
-      $more.show();
-    }
-  })
-  .catch(function (e) {
-    console.log(`BRAND ERROR: ${e}`);
   });
+
+  // rendering list
+  setData(data);
 }
 
-function addChannel() {
+function setData (data) {
+  // API set data
+  let length = data.length;
+  let $list = $articles.find('li');
+
+  // Insert template into skeleton
+  for (let i = 0; i < length; i++) {
+    let tmpImg = `
+      <img data-original="${data[i].image_cover}" width="100%" height="auto" alt="${data[i].title}">
+      <div class="author hide show-lg">${toDateString(data[i].published_at)} | by ${data[i].author}</div>
+    `;
+    let tmpDetail = `
+      <div class="date hide-md">${toDateString(data[i].published_at)}</div>
+      <a class="title" data-module="dotdotdot" href="${data[i].link}" target="_blank">
+        <h3>${data[i].title}</h3>
+      </a>
+    `;
+
+    let itemIndex = braFrom + i;
+    $list.eq(itemIndex).replaceWith(makeBrandItem({
+      link: data[i].link,
+      cover: tmpImg,
+      detail: tmpDetail
+    }));
+  }
+
+  // If length < braSize, delete unnecessary skeleton
+  if (length < braSize) {
+    for (let j = length; j < braSize; j++) {
+      let itemIndex = braFrom*braSize + j;
+      $list.eq(itemIndex).remove();
+    }
+    $more.hide();
+  }
+  else {
+    $more.show();
+  }
+
+}
+
+async function addChannel() {
   // 插入下一頁
   $more.hide();
   braSkeleton();
   braFrom = braFrom + braSize;
-  braGet(braTag, braSize, braFrom);
+  await braGet(braTag, braSize, braFrom);
+  // 多出來的字變...
+  dotdotdot();
   // 圖片 lazyload
   lazyLoad.update();
 }
 
 export function nextPage() {
-
-  braSkeleton();
 
   lazyLoad = new LazyLoad({
     elements_selector: ".braList img"
@@ -103,6 +105,5 @@ export function nextPage() {
   $more.click(addChannel);
 
   // Fetching at first time.
-  braGet(braTag, braSize, braFrom);
-
+  addChannel();
 }
--- src/js/pages/search/nextPage.js
+++ src/js/pages/search/nextPage.js
@@ -7,8 +7,8 @@ let $articles = $channel.find('.srhArticles');
 let $tag = $channel.find('.searchTxt');
 let $notFound = $channel.find('.notFound');
 let $hasFound = $channel.find('.hasFound');
-let srhFrom = 0;
 let srhSize = 12;
+let srhFrom = 0 - srhSize; // fixed for first time loading
 let srhTag = $.trim($tag.text());
 let stopMLoad = false;
 let lazyLoad;
@@ -36,84 +36,84 @@ function makeSearchItem(data) {
   `;
 }
 
-function srhGet(q, size, from) {
-  axios.get(`//${API_HOST}/v1/search/articles`, {
+async function srhGet(q, size, from) {
+  const {data} = await axios.get(`//${API_HOST}/v1/search/articles`, {
     params: {
       q: q,
       size: size,
       from: from
     }
-  })
-  .then(function (response) {
-    // API set data
-    let data = response.data;
-    let length = data.length;
-    let $list = $articles.find('li');
-    // Check whether has articles been found or not at first time
-    if (srhFrom === 0 && length === 0) {
-      // No data
-      $hasFound.remove();
-      $notFound.removeClass('hide');
-      return;
-    }
-    else {
-      // Has data
-      $notFound.remove();
-    }
-    for (let i = 0; i < length; i++) {
-      let tmpImg = `
-        <img data-original="${data[i].image_cover}" width="100%" height="auto" alt="${data[i].title}">
-      `;
-      let tmpDetail = `
-        <span class="date">${toDateString(data[i].published_at)}</span>
-        <span class="author hide show-md">by ${data[i].author}</span>
-      `;
-
-      let itemIndex = srhFrom + i;
-      $list.eq(itemIndex).replaceWith(makeSearchItem({
-        title: data[i].title,
-        link: data[i].link,
-        summary: data[i].summary,
-        cover: tmpImg,
-        detail: tmpDetail
-      }));
-    }
-    dotdotdot();
-    lazyLoad.update();
-    // If length < srhSize, delete unnecessary skeleton
-    if (length < srhSize) {
-      for (let j = length; j < srhSize; j++) {
-        let itemIndex = srhFrom*srhSize + j;
-        $list.eq(itemIndex).remove();
-      }
-      $more.hide();
-      // If length < srhSize, stop mobile auto loading
-      stopMLoad = true;
-    }
-    else {
-      $more.show();
-    }
-  })
-  .catch(function (e) {
-    console.log(`SEARCH ERROR: ${e}`);
   });
+
+  // Check whether has articles been found
+  if (!data.length) {
+    // No data
+    $hasFound.remove();
+    $notFound.removeClass('hide');
+    return;
+  }
+
+  // rendering list
+  setData(data);
 }
 
-function addChannel() {
+function setData(data) {
+  // API set data
+  let length = data.length;
+  let $list = $articles.find('li');
+
+  // Insert template into skeleton
+  for (let i = 0; i < length; i++) {
+    let tmpImg = `
+      <img data-original="${data[i].image_cover}" width="100%" height="auto" alt="${data[i].title}">
+    `;
+    let tmpDetail = `
+      <span class="date">${toDateString(data[i].published_at)}</span>
+      <span class="author hide show-md">by ${data[i].author}</span>
+    `;
+
+    let itemIndex = srhFrom + i;
+    $list.eq(itemIndex).replaceWith(makeSearchItem({
+      title: data[i].title,
+      link: data[i].link,
+      summary: data[i].summary,
+      cover: tmpImg,
+      detail: tmpDetail
+    }));
+  }
+
+  // If length < srhSize, delete unnecessary skeleton
+  if (length < srhSize) {
+    for (let j = length; j < srhSize; j++) {
+      let itemIndex = srhFrom*srhSize + j;
+      $list.eq(itemIndex).remove();
+    }
+    $more.hide();
+    // If length < srhSize, stop mobile auto loading
+    stopMLoad = true;
+  }
+  else {
+    // Show more button after loading all articles in 1 page
+    $more.show();
+  }
+
+}
+
+async function addChannel() {
   // 插入下一頁
   $more.hide();
   srhSkeleton();
   srhFrom = srhFrom + srhSize;
-  srhGet(srhTag, srhSize, srhFrom);
+  await srhGet(srhTag, srhSize, srhFrom);
   // 插入下一頁觸發圖片
   $channel.append(`<img class="infinite" data-original="./src/img/tracker.png" width="0" height="0">`);
+  // 多出來的字變...
+  dotdotdot();
   // 圖片 lazyload
   lazyLoad.update();
 }
 
 export function nextPage() {
-
-  srhSkeleton();
 
   // Lazyload and Next Page
   lazyLoad = new LazyLoad({
@@ -132,5 +132,5 @@ export function nextPage() {
   $more.click(addChannel);
 
   // Fetching at first time.
-  srhGet(srhTag, srhSize, srhFrom);
+  addChannel();
 }
