5fc533b1a4efebc3e2735ca3f9e8ba02e5a413e3
Anny <anny@juksy.com>
2017-06-06 19:43:04 +0800

    修正部分 css, js

--- src/js/pages/idle/index.js
+++ src/js/pages/idle/index.js
@@ -1,12 +1,9 @@
 import { dotdotdot } from '../../common/module.js';
-import { ui } from '../../common/ui.js';
 import { popular } from './popular.js';
 
 const booting = function() {
   // MODULE
   dotdotdot();
-  // UI
-  ui();
   // popular
   popular();
 };
